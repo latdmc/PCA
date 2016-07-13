e14e5533a7c5353fa79abef79d3eaccaf3244f38
ÂäâÁ•ê‰ªª <latdmc@liuyourendemacbook-pro.local>
2016-07-12 21:43:24 -0700

    Initial Commit

---
+++ opencvExample/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  opencvExample
+//
+//  Created by 劉祐任 on 7/12/16.
+//  Copyright © 2016 劉祐任. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
